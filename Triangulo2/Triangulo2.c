e9f50dea01766be09a061519cf8d38ad7f85c647
HectorFlacoyos <rotceh.t.p@gmail.com>
2014-03-25 13:37:22 -0600

    Programa4: Triangulo2

---
+++ Triangulo2/Triangulo2.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int main()
+{
+ int x; 
+ int i;
+ int j;
+
+  printf("Dame un numero:");
+  scanf("%d",&x); 
+
+   for(i=x;i>0;i--){
+
+	   for(j=0;j<i;j++){
+       printf("*");
+       }
+	   
+      printf("\n");          
+  }
+
+  return 0;
+}
